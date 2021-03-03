08ad03c44502d28ffa21d0d61c452cf9d5a01521
Erik  de Jesus Mota Galindo <kenshimota@gmail.com>
2021-03-02 20:36:19 -0400

    the last changed

--- src/fetchSync.js
+++ src/fetchSync.js
@@ -1,4 +1,13 @@
 const fetch = require('node-fetch');
+const { Queue } = require("./queue");
+const { xml2json } = require("xml-js");
+const nodes = Queue();
+
+/* funcion que controla las solicitudes */
+const sleep = function (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
 
 /* estaleciendo los parametros principales para el
 enviar los parametros de forma que podamos enivarlos al servidor
@@ -7,17 +16,32 @@ const getParams = (params = {}, prefix = null) => {
     let body = "";
     let string = "";
     for (var index in params) {
-        
+
         // procesando parametros
         switch (typeof params[index]) {
             case "undefined": string = `${index}=`; break;
-            case "object": string = `${index}=${encodeURIComponent( JSON.stringify(params[index]) )}`; break;
+            case "object": string = `${index}=${encodeURIComponent(JSON.stringify(params[index]))}`; break;
             default: string = prefix != null ? `${prefix}[${index}]=${params[index]}` : `${index}=${params[index]}`; break;
         }
 
         body += body.length === 0 ? string : `&${string}`;
     }
     return body;
+};
+
+/* funcion que se encarga de mostrar el reporte de un error 500 */
+const reportError = function (data) {
+    let str = data.search('id="-lucee-err"') + 180;
+    let exit = "</td>";
+    let count = 0;
+    let output = "";
+
+    for (let i = str; count < exit.length; i++) {
+        output += data[i];
+        count = (exit[count] == data[i] ? count + 1 : 0);
+    }
+
+    return output.slice(0, output.length - exit.length);
 };
 
 /* funcion que se encarga de enviar los parametros obtenidos 
@@ -33,37 +57,48 @@ const fetchSync = async (url, options = {}) => {
         // permite crear parametros en forma de json a procesador
         if (typeof options.body == "object" && options.body != null && options.method == "POST")
             options.body = getParams(options.body);
-        
-        console.log( options, "\n\n");
 
         // let start = Date.now();
-        // console.log(`%c Solicitando Datos de [${options.method}] -> ${url} `, "color: #23b;");
+        !globalThis.window ? console.log("\x1b[36m%s\x1b[0m", `Solicitando Datos de [${options.method}] -> ${url}`) : console.log(`%c Solicitando Datos de [${options.method}] -> ${url}`, "color: #23b;");
+        console.log(options, "\n\n");
+
+        let key = `${nodes.size()} - ${url}`;
+        nodes.add({ key });
+        let aux = nodes.front();
+
+        // realizando la espera 10ms evaluando si la llamada fue realizada
+        while (!nodes.empty() && aux.key != key) {
+            await sleep(10);
+            aux = nodes.front();
+        }
 
         // realizando solicitud de datos desde un fetch determinado
-        let result = await fetch(url, options).then(async (response) =>{
+        let result = await fetch(url, options).then(async (response) => {
             try {
+                let data = await response.text();
+
+
                 // let end = Date.now();
 
                 // notificando que ha ocurrido todo un error
-                // del lado del servidor
-                if (((response.status >= 400 && response.status != 404 && response.status < 500) || response.status <= 200))
-                    throw await response.json().then((r) => {
-                        return r;
-                    }).catch((error) => {
-                        throw error;
-                    });
-                else if( response.status == 404 )
-                    throw  {message: "Not Found Server"};
-                else if( response.status == 500 )
-                    throw { message: response.text() };
+                if (response.status == 500)
+                    throw { error: reportError(data) };
 
                 // console.log(`%c ${response.status} - [${options.method}] -> ${url} obtuvo un respuesta exitosa en ${(end - start)}ms `, "color: green;");
-
                 // retorna el procesamiento de datos
-                return {
-                    response: response.status == 204 ? null : await response.json().then((r) => r),
-                    status: response.status
-                };
+
+                // retornando datos en json
+                switch (options.type) {
+                    case "json":
+                        data = JSON.parse(JSON.parse(data));
+                        if (!data.error == false)
+                            throw data.errormessage;
+                        break;
+                    case "xml": data = JSON.parse(xml2json(data)); break;
+                }
+
+
+                return { response: data, status: response.status };
             } catch (error) {
                 throw error;
             }
@@ -74,13 +109,12 @@ const fetchSync = async (url, options = {}) => {
         return result;
     } catch (error) {
         console.error(error);
-        console.log(`%c Error - [${options.method}] -> ${url} obtuvo un respuesta fallida `, "color: red;");
         throw error;
         return { status: 422, response: error };
     } finally {
-
+        nodes.pop();
     }
 };
 
 
-module.exports = {getParams, fetchSync};
+module.exports = { getParams, fetchSync };
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const {QPM} = require("../index");
+const { QPM } = require("../index");
 
 
 // esta es la variable cliente
@@ -8,22 +8,136 @@ const Client = QPM({
     "server": "http://45.79.44.19:8888" // link and port the server
 });
 
-// ---------- Main -----------
-async function main(){
 
-    await Client.addEditAssetClass({
+// esta funcion se encarga de buscar que methodos hacen falta por documentar
+Client.getMethodsNotDefined().then(console.log);
+
+// ---------- Main -----------
+async function main() {
+
+    Client.method("addEditAssetClass", {
         classinfo: {
             site_name: "San Jose CR",
             asset_class: "Test1 - erik",
             asset_description: "Description - test - 1 ",
-            asset_text: "Prueb - 1"
+            asset_text: "1",
+            level: 2
         }
     });
 
-};
+    // solicitando datos del api
+    let asset = await Client.fetch().then((t) => t);
+    console.log("asset", asset);
+
+    // cambiando de llamada
+    Client.method("addClassVariable", {
+        variableinfo: {
+            "site_name": "San Jose CR",
+            "asset_class": "Test1 - erik",
+            "level": 2,
+            "variable_description": "Estoy describiendo",
+            "variable_name": "var1",
+            "variable_sqltype": "varchar",
+            "variable_value": "TEST"
+        }
+    });
+
+    // solicitando los datos del api de la siguiente llamada
+    let variable = await Client.fetch().then((t) => t);
+    console.log("variable", variable);
+
+    // obteniendo la lista de clases de un sitio determinado
+    await Client.method("listAllPrimaryClassesFromSite", {
+        sitename: { site_name: "San Jose CR" }
+    }).fetch().then(console.log);
+
+    // obteniendo la lista de clases de un sitio determinado
+    await Client.method("listAllPrimaryClassesFromSite", {
+        sitename: { site_name: "San Jose CR" }
+    }).fetch().then(console.log);
+
+    // obtenemos la listad de board de un sitio determinado
+    let defaultBoard = await Client.method("getDefaultBoardFromSite", {
+        sitename: { site_name: "San Jose CR" },
+    }).fetch().then((response) => response);
+
+    // agregando 
+    let board = await Client.method("addEditBoard", {
+        boardinfo: {
+            "site_name": "San Jose CR",
+            "board_type": "Tipo1",
+            "board_thickness": 2000.12,
+            "board_unit": 1,
+            "board_weight": 200.00
+        }
+    }).fetch().then((response) => response);
+    console.log(board);
+
+    // obtiene la informacion de la cubeta
+    await Client.method("getBoardInfo", {
+        boardid: { id: defaultBoard.id }
+    }).fetch().then(console.log);
+
+    // eliminando un board
+    await Client.method("deleteBoards", {
+        boardids: {
+            boardids: [{ id: board.id }]
+        }
+    }).fetch().then(console.log);
+
+    await Client.method("getRSCDrawing", {
+        boxdata: {
+            "width": 1000,
+            "height": 1000,
+            "box_extlength": 200,
+            "box_extwidth": 400,
+            "box_extheight": 123.12,
+            "box_color": "azul",
+            "box_thick": 12398.4
+        }
+    }).fetch("text").then(console.log);
+
+    await Client.method("listBoardsPerSite", {
+        siteinfo: { site_name: "San Jose CR" }
+    }).fetch().then(console.log);
+
+    // obteniendo todas la clases
+    /*await Client.method("getVariablesFromClassID", {
+        classid: { assetclass_id: asset.id }
+    }).fetch().then(console.log);
+
+    // obteniendo todas la clases
+    await Client.method("getAllFromClass", {
+        classinfo: {
+            site_name: "San Jose CR",
+            asset_class: "Test1 - erik",
+            level: 2
+        }
+    }).fetch().then(console.log);
 
 
-main();
+    // eliminando la variable recien creada anteriormente
+    await Client.method("deleteClassVariable", {
+        variableid: { id: variable.id },
+    }).fetch().then(console.log);
+
+    // eliminando la variable todas la variables
+    await Client.method("deleteAllVariablesForClass", {
+        classname: {
+            site_name: "San Jose CR",
+            asset_class: "Test1 - erik",
+            level: 2
+        }
+    }).fetch().then(console.log);
+
+    // eliminando los datos del acces class
+    await Client.method("deleteAssetClass", {
+        assetid: { id: asset.id }
+    }).fetch().then(console.log);*/
+
+}
+
+// main();
 
 
 
