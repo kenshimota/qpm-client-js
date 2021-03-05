fa6bb566ec12ce2198e5436eb48339ef2b535f13
Erik  de Jesus Mota Galindo <kenshimota@gmail.com>
2021-03-05 00:11:01 -0400

    exporting cdn

--- src/qpm-methods.js
+++ src/qpm-methods.js
@@ -2,15 +2,17 @@ const boards = require("./methods/boards");
 const classes = require("./methods/classes");
 const packages = require("./methods/packages");
 const boxes = require("./methods/boxes");
+let packagesMethods = { boards, classes, packages, boxes };
+let methods = {};
 
 // methodos expuesto
-const methods = { 
-	...boards, ...classes, ...packages, 
-	...boxes 
-};
+for (let i in packagesMethods) {
+    for (let j in packagesMethods[i])
+        methods[j] = packagesMethods[i][j];
+}
 
 const reports = [
-    "/QPMCalcServer/cfc/QPMAssetClassService.cfc?wsdl", 
+    "/QPMCalcServer/cfc/QPMAssetClassService.cfc?wsdl",
     "/QPMCalcServer/cfc/QPMBoardService.cfc?wsdl",
     "/QPMCalcServer/cfc/QPMPackageService.cfc?wsdl",
     "/QPMCalcServer/cfc/QPMBoxService.cfc?wsdl",
