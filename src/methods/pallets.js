a36a4bb7ea942b3cb9484988cdd5849ad383d5f4
Erik  de Jesus Mota Galindo <kenshimota@gmail.com>
2021-03-25 09:40:13 -0400

    change loaders

--- src/methods/pallets.js
+++ src/methods/pallets.js
@@ -1,120 +1,124 @@
 const pallets = {
-    "addEditPalletConstraints": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=addEditPalletConstraints"
-    },
+  addEditPalletConstraints: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=addEditPalletConstraints',
+  },
 
-    "addEditSlab": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=addEditSlab"
-    },
+  addEditSlab: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=addEditSlab',
+  },
 
-    "deleteEmptyPallet": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteEmptyPallet"
-    },
+  deleteEmptyPallet: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteEmptyPallet',
+  },
 
-    "deleteFullPallet": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteFullPallet"
-    },
+  deleteFullPallet: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteFullPallet',
+  },
 
-    "deleteFullPallets": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteFullPallets"
-    },
+  deleteFullPallets: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteFullPallets',
+  },
 
-    "deletePallets": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deletePallets"
-    },
+  deletePallets: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deletePallets',
+  },
 
-    "deleteSlab": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteSlab"
-    },
+  deleteSlab: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteSlab',
+  },
 
-    "deleteSlabs": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteSlabs"
-    },
+  deleteSlabs: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=deleteSlabs',
+  },
 
-    "getConstraintsPerSlab": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getConstraintsPerSlab"
-    },
+  getConstraintsPerSlab: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getConstraintsPerSlab',
+  },
 
-    "getContentsFromPalletFill": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getContentsFromPalletFill"
-    },
+  getContentsFromPalletFill: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getContentsFromPalletFill',
+  },
 
-    "getFullPalletContents": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getFullPalletContents"
-    },
+  getFullPalletContents: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getFullPalletContents',
+  },
 
-    "getPalletInfo": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getPalletInfo"
-    },
+  getPalletInfo: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getPalletInfo',
+  },
 
-    "getSlabInfo": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getSlabInfo"
-    },
+  getSlabInfo: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getSlabInfo',
+  },
 
-    "listFullPalletsPerSite": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listFullPalletsPerSite"
-    },
+  listFullPalletsPerSite: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listFullPalletsPerSite',
+  },
 
-    "listPalletNamesPerSite": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listPalletNamesPerSite"
-    },
+  listPalletNamesPerSite: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listPalletNamesPerSite',
+  },
 
-    "listPalletsPerSite": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listPalletsPerSite"
-    },
+  listPalletsPerSite: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listPalletsPerSite',
+  },
 
-    "listSlabsPerSite": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listSlabsPerSite"
-    },
+  listSlabsPerSite: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=listSlabsPerSite',
+  },
 
-    "queryOrphanPalletIDs": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=queryOrphanPalletIDs"
-    },
+  queryOrphanPalletIDs: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=queryOrphanPalletIDs',
+  },
 
-    "fillPalletFromBox": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=fillPalletFromBox"
-    },
+  fillPalletFromBox: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=fillPalletFromBox',
+  },
 
-    "fillPalletSequence": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=fillPalletSequence"
-    },
+  fillPalletSequence: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=fillPalletSequence',
+  },
 
-    "getPalletArrangement": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getPalletArrangement"
-    },
+  getPalletArrangement: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=getPalletArrangement',
+  },
 
-    "palletFillFromDims": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletCalc.cfc?method=palletFillFromDims"
-    },
+  palletFillFromDims: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletCalc.cfc?method=palletFillFromDims',
+  },
 
-    "palletFillsFromShipmentDims": {
-        method: "POST",
-        uri: "/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=palletFillsFromShipmentDims"
-    },
+  palletFillsFromShipmentDims: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/QPMPalletService.cfc?wsdl&method=palletFillsFromShipmentDims',
+  },
 
-    
+  fullPalletSVGFromData: {
+    method: 'POST',
+    uri: '/QPMCalcServer/cfc/SVGPalletCalcs.cfc?method=fullPalletSVGFromData',
+    responseType: 'text',
+  },
 };
 
 module.exports = pallets;
