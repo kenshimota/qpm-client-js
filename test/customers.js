221fc0701fb2b45317f70667f09461618aa1ef19
Erik Mota Galindo <kenshimota@gmail.com>
2021-10-20 18:15:43 -0400

    hidden information that connect to serve

--- test/customers.js
+++ test/customers.js
@@ -1,14 +1,7 @@
 async function main() {
   const { QPM } = require('../index');
-  const params = {
-    username: 'tsancio_cr',
-    password: 'koona',
-    server: 'http://45.79.44.19:8888',
-    calcdb: 'qpm_calcdb',
-    ctrldb: 'qpm_controldb',
-    vro_calcdb: 'vro_calcdb',
-  };
-
+  const params = require("../config/data.json");
+  
   // esta es la variable cliente
   const Client = QPM({});
   const data = await Client.login(params).then(t => t);
